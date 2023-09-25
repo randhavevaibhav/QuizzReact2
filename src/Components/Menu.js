aabfa1ca06a0a18a4bd9f4ed1d16c7a6ba030d46
Vaibhav Randhave <randhavevaibhav3@gmail.com>
2023-09-25 10:38:56 +0530

    added check for player name

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,6 +4,16 @@ import { GameStateContex } from "../Helpers/Contex";
 function Menu ()
 {
     const {gameState, setGameState, userName, setuserName} = useContext(GameStateContex);
+    function checheckUsername(checkName)
+    {
+            if(checkName===undefined || checkName==="")
+            {
+                alert("Please enter your Name !!");
+            }
+            else{
+                setGameState("playing");
+            }
+    }
 
     return (
 <div className="Menu">
@@ -11,7 +21,7 @@ function Menu ()
 <input type="text" placeholder="Ex. Jhon Doe" onChange={(event)=>{
     setuserName(event.target.value)
 }}></input>
-<button onClick={()=>{setGameState("playing")}}>Satrt Quizz</button>
+<button onClick={()=>{ checheckUsername(userName)}}>Satrt Quizz</button>
 </div>
 
     );
